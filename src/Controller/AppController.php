165ee37c9cc3c920e4941c08c1b5ca44a35b3217
Magdy <magdy.yackoub@rapid-develop.com>
2017-06-27 23:01:14 +0200

    User login

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,7 +43,22 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
-
+        if ($this->request->params['prefix'] === 'admin') {
+            $this->loadComponent('Auth', [
+                'authenticate' => [
+                    'Form' => [
+                        'fields' => [
+                            'username' => 'email',
+                            'password' => 'password'
+                        ]
+                    ]
+                ],
+                'loginAction' => [
+                    'controller' => 'Users',
+                    'action' => 'login'
+                ]
+            ]);
+        }
         /*
          * Enable the following components for recommended CakePHP security settings.
          * see http://book.cakephp.org/3.0/en/controllers/components/security.html
