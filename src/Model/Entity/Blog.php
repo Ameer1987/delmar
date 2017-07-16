565091aaee17c89f325c4470468722aaf92a9c28
Magdy <magdy.yackoub@rapid-develop.com>
2017-07-16 16:30:56 +0200

    Empty blog_id

--- src/Model/Entity/Blog.php
+++ src/Model/Entity/Blog.php
@@ -8,16 +8,31 @@ use Cake\ORM\Entity;
  *
  * @property int $id
  * @property int $order
- * @property string $title
- * @property string $photo
- * @property string $dir
- * @property string $text
+ * @property string $title_1
+ * @property string $title_2
+ * @property string $title_3
+ * @property string $desc_1
+ * @property string $desc_2
+ * @property string $desc_3
+ * @property $main_img
+ * @property string $main_dir
+ * @property $img_1
+ * @property string $dir_1
+ * @property $img_2
+ * @property string $dir_2
+ * @property $img_3
+ * @property string $dir_3
+ * @property $img_4
+ * @property string $dir_4
  * @property string $locale
+ * @property \Cake\I18n\FrozenTime $created_at
  *
+ * @property \App\Model\Entity\BlockTab[] $block_tabs
  * @property \App\Model\Entity\HomeBox[] $home_boxes
  * @property \App\Model\Entity\HomeSlider[] $home_sliders
  * @property \App\Model\Entity\LargeImageBox[] $large_image_boxes
  * @property \App\Model\Entity\ResponsiveImageBox[] $responsive_image_boxes
+ * @property \App\Model\Entity\Service[] $services
  * @property \App\Model\Entity\SmallImageBox[] $small_image_boxes
  * @property \App\Model\Entity\SocialLink[] $social_links
  */
